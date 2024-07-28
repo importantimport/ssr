f231e340f128209e8688a36e1532d3deacf710d4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-28 17:04:44 +0800

    chore: add vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import unoCSS from '@unocss/vite'
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 import unoConfig from './uno.config'
 
@@ -9,6 +10,7 @@ import unoConfig from './uno.config'
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
     unoCSS(unoConfig),
   ],
   resolve: {
